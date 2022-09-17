d7e10d2fb19dca5ccdef8a406663ee6762217b7e
Carl Lippert <carl.lippert@gmail.com>
2022-09-17 15:43:57 +0200

    boiler next auth

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,40 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
+import '@rainbow-me/rainbowkit/styles.css'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
+// import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+import { RainbowKitSiweNextAuthProvider } from '@rainbow-me/rainbowkit-siwe-next-auth'
+import { SessionProvider } from 'next-auth/react';
+
+
+const { chains, provider } = configureChains(
+  [chain.polygon, chain.optimism, chain.arbitrum],
+  [publicProvider()]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'zkLeap',
+  chains,
+})
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <SessionProvider refetchInterval={0} session={pageProps.session}>
+        <RainbowKitSiweNextAuthProvider>
+          <RainbowKitProvider chains={chains}>
+            <Component {...pageProps} />
+          </RainbowKitProvider>
+        </RainbowKitSiweNextAuthProvider>
+      </SessionProvider>
+    </WagmiConfig>
+  )
 }
