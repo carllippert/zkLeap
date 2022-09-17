6b5cae44c5903a31504a1391f92a476cb02f99aa
Carl Lippert <carl.lippert@gmail.com>
2022-09-17 09:30:41 +0200

    marketig boilerplate.

---
+++ src/pages/index.jsx
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+
+import { CallToAction } from '@/components/CallToAction'
+import { Faqs } from '@/components/Faqs'
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { Hero } from '@/components/Hero'
+import { Pricing } from '@/components/Pricing'
+import { PrimaryFeatures } from '@/components/PrimaryFeatures'
+import { SecondaryFeatures } from '@/components/SecondaryFeatures'
+import { Testimonials } from '@/components/Testimonials'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>zkLeap - Find your next job, privately.</title>
+        <meta
+          name="description"
+          content="Find your next job, privately"
+        />
+      </Head>
+      <Header />
+      <main>
+        <Hero />
+        <PrimaryFeatures />
+        <SecondaryFeatures />
+        <CallToAction />
+        <Testimonials />
+        <Pricing />
+        <Faqs />
+      </main>
+      <Footer />
+    </>
+  )
+}
